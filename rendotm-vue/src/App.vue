fac3206615b91c5339a759f0f94b6c723b18ba4c
Chris Chung <jjsung@users.noreply.github.com>
2020-02-17 23:37:36 -0500

    added logic for handling post and get by id

--- rendotm-vue/src/App.vue
+++ rendotm-vue/src/App.vue
@@ -11,20 +11,50 @@
     </header>
     <main>
       <div>
-        <h1 align="left">Get</h2>
+        <h1 align="left">Get</h1>
         <vue-good-table
           :columns="columns"
           :rows="calcRow"/>
       </div>
       <div align="left"> 
-        <h1 align="left">POST</h2>
-        <md-button
-          @click="$emit('click', true)"
-        >
-          Post
-      </md-button>
+        <h1 >POST</h1>
+        <!-- <form id="signup-form">
+          <div class="field">
+            <label class="label">User ID</label>
+            <input type="text" class="input" name="userID">
+          </div>
+          <div class="field">
+            <label class="label">Name</label>
+            <input type="text" class="input" name="name">
+          </div>
+          <div class="field">
+            <label class="label">Password</label>
+            <input type="text" class="input" name="password">
+          </div>
+          <div class="field">
+            <label class="label">Tenanats</label>
+            <input type="text" class="input" name="tenanats">
+          </div> -->
+          <div class="field has-text-right">
+            <button @click="SendForm" type="text" class="button is-danger">Submit</button>
+          </div>
+        <!-- </form> -->
       </div>
-
+      <div align="left"> 
+        <h1 >GET By ID</h1>
+            <button @click="GetById('1')" type="text" class="button is-danger">Submit</button>
+            {{getViaId}}
+      </div>
+        <div align="left"> 
+        <h1 >POST By ID</h1>
+            <button @click="PostById('1')" type="text" class="button is-danger">Submit</button>
+            {{postById}}
+      </div>
+        <div align="left"> 
+        <h1 >DELETE By ID</h1>
+            <button @click="DeleteById('1')" type="text" class="button is-danger">Submit</button>
+            {{deleteById}}
+      </div>
       <router-view></router-view>
     </main>
   </div>
@@ -56,9 +86,78 @@ export default {
         }
       ],
       info: '',
-      errored: false
+      errored: false,
+      getViaId: '',
+      postById: '',
+      deleteById: '',
     }
   },
+  methods: {
+    SendForm(payload) {
+      console.log('SendForm')
+      console.log('payload' + JSON.stringify(payload))
+      axios.post('http://localhost:8081/users',
+        {
+          user_id: '3',
+          username: 'cc@gmail.com',
+          password: '4445',
+          tenants: [1, 2, 3]
+        },
+        
+      )
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    },
+    GetById(payload) {
+        console.log('GetById')
+        console.log('payload' + JSON.stringify(payload))
+        axios.get('http://localhost:8081/users/'+ payload)
+        .then(response => (this.getViaId = response.data)).catch(error => {
+          console.log(error)
+          this.errored = true
+        })
+      },
+    },
+    PostById(payload) {
+      console.log('PostById')
+      console.log('payload' + JSON.stringify(payload))
+      axios.post('http://`localhost`:8081/users/1',
+        {
+          user_id: 'PostById',
+          username: 'cc@gmail.com',
+          password: '4445',
+          tenants: [1, 2, 3]
+        }
+      )
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    },
+    DeleteById(payload) {
+      console.log('PostById')
+      console.log('payload' + JSON.stringify(payload))
+      axios.delete('http://`localhost`:8081/users/1',
+        {
+          user_id: 'PostById',
+          username: 'cc@gmail.com',
+          password: '4445',
+          tenants: [1, 2, 3]
+        }
+      )
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    },
   mounted () {
     axios
       .get('http://localhost:8081/users')
